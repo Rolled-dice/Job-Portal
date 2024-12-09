823e755d5b561d20aabe81cb3dc14edb7bbeb8ef
@MOHIT_DUTT <60535467+Rolled-dice@users.noreply.github.com>
2024-12-09 23:34:52 +0530

    Update app-layout.jsx

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -7,6 +7,7 @@ const AppLayout = () => {
       <div className="grid-background "></div>
       <main className="min-h-screen px-10 ">
         <Header />
+
         <Outlet />
       </main>
        {/*  <div className="p-10 text-center bg-gray-800 mt-10">
