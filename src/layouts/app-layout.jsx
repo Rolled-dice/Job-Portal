642b9c17712c0b7e3456e26d4409118f5e8c42f3
@MOHIT_DUTT <60535467+Rolled-dice@users.noreply.github.com>
2024-12-09 23:21:25 +0530

    Update app-layout.jsx

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -9,8 +9,8 @@ const AppLayout = () => {
         <Header />
         <Outlet />
       </main>
-      <div className="p-10 text-center bg-gray-800 mt-10">
-        {/* Made with 💗 by Mohit */}
+       {/*  <div className="p-10 text-center bg-gray-800 mt-10">
+       Made with 💗 by Mohit */}
       </div>
     </>
   );
